3f4a6d6cc483ee392ee49cea440e53757c2f40b2
zuramai <ahmadsaugigis@yahoo.com>
2021-07-26 10:37:46 +0700

    add: moves history card

--- src/components/game/GameStats.vue
+++ src/components/game/GameStats.vue
@@ -3,6 +3,16 @@
         <h1 class='text-6xl text-white '>{{ capitalizeFirstLetter(turn) }}</h1>
         <p class='text-xl text-light-800'>turn</p>
     </div>
+    <div class="game-stats w-full bg-[#2b2b2b] p-8 rounded-lg mt-5">
+        <h3 class='text-lg text-white font-bold'>Moves history</h3>
+        <div class="moves-history">
+            <ul>
+                <li class='flex'>
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 <script setup>
 import { computed } from "@vue/runtime-core";
@@ -14,3 +24,10 @@ let turn = computed(() => store.state.turn)
 
 const capitalizeFirstLetter = (string) =>  string[0].toUpperCase() + string.slice(1)
 </script>
+<style lang="scss">
+    .moves-history {
+        min-height: 300px;
+        max-height: 400px;
+        overflow-y: hidden;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,10 @@
     <div class="page-home">
         <div class="container mx-auto py-12">
             <div class="flex flex-wrap">
-                <div class="w-full px-5 md:w-2/3">
+                <div class="w-full px-5 md:w-7/12">
                     <Game/>
                 </div>
-                <div class="w-full px-5 md:w-1/3">
+                <div class="w-full px-5 md:w-5/12">
                     <GameStats/>
                 </div>
             </div>
